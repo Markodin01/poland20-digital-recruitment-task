2ec918e15f885a73bdffe5fa90af03bc1fc904cd
Marek Gajewski <marek.gajewski2001@gmail.com>
2025-02-14 17:40:47 +0100

    add pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
+		output: 'export',
+		basePath: '/poland20-digital-recruitment-task',
 		formats: ['image/avif', 'image/webp'],
 		dangerouslyAllowSVG: true,
 		contentDispositionType: 'attachment',
